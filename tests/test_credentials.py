e3fab97ebb2ececa2628211e437ee8d4c3350e78
dominikwalk <dominik.walk@mail.de>
2022-10-04 19:54:45 +0200

    Add test for illegal user config path creation

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -3,13 +3,19 @@ from pathlib import Path
 import pytest
 
 from quick_gist.credentials import _check_user_config_existance
+from quick_gist.credentials import _create_user_config_dir
 from quick_gist.credentials import _password_decrypt
 from quick_gist.credentials import _password_encrypt
 from quick_gist.credentials import crypto_iterations
 from quick_gist.credentials import UserCredentialsError
 from quick_gist.credentials import UserOsError
 
-TEST_CONFIG_PATH = Path("./.config/quick_gist/config.yaml")
+
+TEST_USER_CONFIG_PATH = "." + "/.config/quick-gist/"
+TEST_USER_CONFIG_NAME = "quick-gist-config.yaml"
+TEST_FULL_CONFIG_PATH = Path(f"{TEST_USER_CONFIG_PATH}{TEST_USER_CONFIG_NAME}")
+
+TEST_ILLEGAL_USER_CONFIG_PATH = "." + "//.config/quick-gist/"
 
 
 def test_password_encrypt_decrypt_valid_password():
@@ -56,10 +62,22 @@ def test_check_user_config_existance():
     # check non existing user_configuration
     with pytest.raises(SystemExit) as exc_info_sys:
         with pytest.raises(UserOsError) as exc_info_custom:
-            _check_user_config_existance(path=TEST_CONFIG_PATH)
+            _check_user_config_existance(path=TEST_FULL_CONFIG_PATH)
 
         assert (
             str(exc_info_custom.value)
             == "Could not find a user configuration (use add-user first)"
+        )
+    assert exc_info_sys.value.code == 1
+
+
+def test_create_user_config_dir_illegal():
+    with pytest.raises(SystemExit) as exc_info_sys:
+        with pytest.raises(UserOsError) as exc_info_custom:
+            _create_user_config_dir(path=TEST_ILLEGAL_USER_CONFIG_PATH)
+
+        assert (
+            str(exc_info_custom.value)
+            == f"Could not create a new user configuraion dir at {ILLEGAL_USER_CONFIG_PATH}"
         )
     assert exc_info_sys.value.code == 1
