79b7ef37f0ee461e1c44677f8131b03813359532
dominikwalk <dominik.walk@mail.de>
2022-10-05 14:39:40 +0200

    Add tests for user configuration dir/file creation

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import pytest
 
 from quick_gist.credentials import _check_user_config_existance
+from quick_gist.credentials import _create_default_user_config
 from quick_gist.credentials import _create_user_config_dir
 from quick_gist.credentials import _password_decrypt
 from quick_gist.credentials import _password_encrypt
@@ -13,16 +14,18 @@ from quick_gist.credentials import crypto_iterations
 from quick_gist.credentials import UserCredentialsError
 from quick_gist.credentials import UserOsError
 
-
 TEST_USER_CONFIG_PATH = Path("." + "/.config/quick-gist/")
 TEST_USER_CONFIG_NAME = "quick-gist-config.yaml"
 TEST_FULL_CONFIG_PATH = Path(f"{TEST_USER_CONFIG_PATH}{TEST_USER_CONFIG_NAME}")
 
 TEST_ILLEGAL_USER_CONFIG_PATH = "." + "//.config/quick-gist/"
+TEST_ILLEGAL_FULL_USER_CONFIG_PATH = Path(
+    f"{TEST_ILLEGAL_USER_CONFIG_PATH}quick/-gist-config-yaml",
+)
 
 
 def test_password_encrypt_decrypt_valid_password():
-
+    """Test password encryption and decryption of a string with the right password"""
     test_message = "secret test message"
     test_password = "testpassword42"
 
@@ -41,7 +44,7 @@ def test_password_encrypt_decrypt_valid_password():
 
 
 def test_password_encrypt_decrypt_wrong_password():
-
+    """Test password encryption and decryption of a string with the wrong password"""
     test_message = "secret test message"
     test_password = "testpassword42"
     wrong_password = "wrong_password"
@@ -61,35 +64,70 @@ def test_password_encrypt_decrypt_wrong_password():
 
 
 def test_check_user_config_existance():
-
+    """Test the check of an existing user configuration file"""
     # check non existing user_configuration
     with pytest.raises(SystemExit) as exc_info_sys:
-        with pytest.raises(UserOsError) as exc_info_custom:
-            _check_user_config_existance(path=TEST_FULL_CONFIG_PATH)
+        _check_user_config_existance(path=TEST_FULL_CONFIG_PATH)
 
-        assert (
-            str(exc_info_custom.value)
-            == "Could not find a user configuration (use add-user first)"
-        )
-    assert exc_info_sys.value.code == 1
+    assert exc_info_sys.errisinstance(UserOsError)
+    assert (
+        str(exc_info_sys.value)
+        == "Could not find a user configuration (use add-user first)"
+    )
 
 
 def test_create_user_config_dir_illegal():
-    with pytest.raises(SystemExit) as exc_info_sys:
-        with pytest.raises(UserOsError) as exc_info_custom:
-            _create_user_config_dir(path=TEST_ILLEGAL_USER_CONFIG_PATH)
+    """Test creatio of user configuration directory at a illegali path"""
+    # with pytest.raises(SystemExit) as exc_info_sys:
+    with pytest.raises(UserOsError) as exc_info_sys:
+        _create_user_config_dir(path=TEST_ILLEGAL_USER_CONFIG_PATH)
 
-        assert (
-            str(exc_info_custom.value)
-            == f"Could not create a new user configuraion dir at {ILLEGAL_USER_CONFIG_PATH}"
-        )
-    assert exc_info_sys.value.code == 1
+    assert exc_info_sys.errisinstance(UserOsError)
+    assert (
+        str(exc_info_sys.value)
+        == f"Could not create a new user configuraion dir at {TEST_ILLEGAL_USER_CONFIG_PATH}"
+    )
 
 
 def test_create_user_config_dir_legal(caplog):
+    """Test creation of user configuation directory"""
     with caplog.at_level(logging.INFO):
         _create_user_config_dir(path=TEST_USER_CONFIG_PATH)
 
-    assert f"Created directory at {TEST_USER_CONFIG_PATH}"
+    assert f"Created directory at {TEST_USER_CONFIG_PATH}" in caplog.text
+    # clean up directory
+    shutil.rmtree(path=Path(".config"))
 
+
+def test_create_user_config_illegal():
+    """Test creation of a new user file at a illegal path"""
+    # create legal config path first
+    _create_user_config_dir(path=TEST_USER_CONFIG_PATH)
+
+    with pytest.raises(SystemExit) as exc_info_sys:
+        _create_default_user_config(path=TEST_ILLEGAL_FULL_USER_CONFIG_PATH)
+
+    assert exc_info_sys.errisinstance(UserOsError)
+    assert str(exc_info_sys.value) == f"Could not create a new user configuration file"
+    # clean up test directory
+    shutil.rmtree(path=Path(".config"))
+
+
+def test_create_user_config(caplog):
+    """Test creation of a new user configuration file"""
+    # create legal config path first
+    _create_user_config_dir(path=TEST_USER_CONFIG_PATH)
+    with caplog.at_level(logging.INFO):
+        _create_default_user_config(path=TEST_FULL_CONFIG_PATH)
+
+    assert f"Created config file at {TEST_FULL_CONFIG_PATH}" in caplog.text
+
+    # clean up test directory
+    shutil.rmtree(path=Path(".config"))
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """Called when whole test run is finished"""
+
+    # clean up test directory in case anthing failed before
     shutil.rmtree(path=Path(".config"))
